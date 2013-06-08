95e632341ad5ef1a8ac0b1afe587ef823b80965a
Filip Procházka <filip@prochazka.su>
2013-06-08 13:52:09 +0200

    Fixed class_alias

--- src/Kdyby/Events/DI/EventsExtension.php
+++ src/Kdyby/Events/DI/EventsExtension.php
@@ -21,7 +21,7 @@ if (!class_exists('Nette\DI\CompilerExtension')) {
 	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
 	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
-	class_alias('Nette\Config\Helpers', 'Nette\DI\Helpers');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
 }
 
 /**
